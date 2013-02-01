e0a146b9b1d785aa99932329c37c53d36beeb754
Kanwei Li <kanwei@gmail.com>
2013-02-01 16:38:04 -0500

    Improve performance 3x by buffering output

--- median/median.clj
+++ median/median.clj
@@ -25,10 +25,14 @@
       (< max-size min-size)
         (.add max-heap (.poll min-heap)))))
 
-(defn solve-rec [lines ^java.util.PriorityQueue max-heap ^java.util.PriorityQueue min-heap answers]
-  ; (println max-heap min-heap (first lines) answers)
-  (if (empty? lines) 
-    answers
+(defn format-answer [x]
+  (if (instance? clojure.lang.Ratio x)
+    (apply str (reverse (drop-while #(= \0 %) (reverse (format "%f" (double x))))))
+    (str x)))
+
+(defn solve-rec [lines ^java.util.PriorityQueue max-heap ^java.util.PriorityQueue min-heap out]
+  ; (println max-heap min-heap (first lines))
+  (when-not (empty? lines)
     (let [rest-lines (rest lines)
           [op n_str] (clojure.string/split (first lines) #" ")
           n (Integer. n_str)]
@@ -36,30 +40,32 @@
         (= op "r")
           (if-not (or (.remove max-heap n) (.remove min-heap n))
             ; Can't find in either heap, move on
-            (recur rest-lines max-heap min-heap (conj answers "Wrong!"))
+            (do
+              (.write out "Wrong!\n")
+              (recur rest-lines max-heap min-heap out))
             (do
               (balance-heap max-heap min-heap)
-              (recur rest-lines max-heap min-heap (conj answers (median max-heap min-heap)))))
+              (.write out (format-answer (median max-heap min-heap)))
+              (.newLine out)
+              (recur rest-lines max-heap min-heap out)))
         (= op "a")
           (do 
             (.add max-heap n)
             (balance-heap max-heap min-heap)
-            (recur rest-lines max-heap min-heap (conj answers (median max-heap min-heap))))))))
+            (.write out (format-answer (median max-heap min-heap)))
+            (.newLine out)
+            (recur rest-lines max-heap min-heap out))))))
 
-(defn format-answer [x]
-  (if (instance? clojure.lang.Ratio x)
-    (apply str (reverse (drop-while #(= \0 %) (reverse (format "%f" (double x))))))
-    (str x)))
-
-(defn solve [src]
+(defn solve [src dest]
   (let [f (line-seq (clojure.java.io/reader src))
-        min-heap (java.util.PriorityQueue.)
-        max-heap (java.util.PriorityQueue. 10 (comparator (fn [x y] (> x y))))
-        answers (solve-rec (rest f) max-heap min-heap [])
-        print-answer (comp println format-answer)]
-    #_(doseq [a answers] (print-answer a))))
+        out (clojure.java.io/writer dest)
+        min-heap (java.util.PriorityQueue. 51000)
+        max-heap (java.util.PriorityQueue. 51000 (comparator (fn [x y] (> x y))))]
+    (solve-rec (rest f) max-heap min-heap out)
+    (.flush out)))
 
 (defn -main []
-  (solve *in*))
+  (solve *in* *out*))
 
-(time (solve "/Users/kanwei/Projects/istreet/median/input00.txt"))
+; (time (solve "/Users/kanwei/Projects/istreet/median/input01.txt" (doto (java.io.File/createTempFile "medians" ".out") .deleteOnExit)))
+; (time (solve "/Users/kanwei/Projects/istreet/median/input01.txt" *out*))
