dd35353e21a49dfae0edd3e01ce77337fd1e901c
Kanwei Li <kanwei@gmail.com>
2013-01-31 21:39:10 -0500

    Faster but still slow

--- median/median.clj
+++ median/median.clj
@@ -1,7 +1,7 @@
 (ns solution (:gen-class))
 (use 'clojure.test)
 
-(defn median-sorted [xs]
+(defn median-sorted-drop [xs]
   (let [len (count xs)
         secondhalf (drop (quot (dec len) 2) xs)]
     (cond 
@@ -9,12 +9,21 @@
       (odd? len) (first secondhalf)
       (even? len) (/ (reduce + (take 2 secondhalf)) 2))))
 
+(defn median-sorted [xs]
+  ; {:pre [(vector? xs)]}
+  (let [len (count xs)
+        v1 (quot (dec len) 2)]
+    (cond 
+      (zero? len) nil
+      (odd? len) (first (subvec xs v1 (inc v1)))
+      (even? len) (/ (reduce + (subvec xs v1 (+ 2 v1))) 2))))
+
 (deftest medians
   (is (= (median-sorted []) nil))
   (is (= (median-sorted [1]) 1))
-  (is (= (median-sorted [1,2]) 3/2))
-  (is (= (median-sorted [1,2,1]) 1))
-  (is (= (median-sorted [1,2,3,4]) 5/2))
+  (is (= (median-sorted [1,2]) 3/2)) ; 0, 2
+  (is (= (median-sorted [1,1,2]) 1)) ; 1, 2
+  (is (= (median-sorted [1,2,3,4]) 5/2)) ; 1, 3
   (is (= (median-sorted [1 2 30 49 80 100]) 79/2))
   (is (= (median-sorted [1,2,3,4,5]) 3)))
 
@@ -25,12 +34,21 @@
      (<= n (first alon)) (into result (cons n alon))
      :else (recur (rest alon) (conj result (first alon))))))
 
-(defn remove-first [el xs]
+(defn remove-first-slow [el xs]
   (let [index_el (.indexOf xs el)]
     (if (neg? index_el)
       xs
       (let [[fsplit ssplit] (split-at index_el xs)]
         (concat fsplit (drop 1 ssplit))))))
+
+(defn remove-first [el xs]
+  ; {:pre [(vector? xs)]}
+  (let [index_el (.indexOf xs el)]
+    (if (neg? index_el)
+      xs
+      (let [left (subvec xs 0 index_el)
+            right (subvec xs (inc index_el))]
+        (into left right)))))
 
 (deftest remove-firsts
   (is (= (remove-first 1 [3,1,2,3,4]) [3,2,3,4]))
@@ -60,7 +78,7 @@
 (defn solve [src]
   (let [f (line-seq (clojure.java.io/reader src))
         nlines (read-string (first f))
-        answers (solve-rec (rest f) '() [])]
+        answers (solve-rec (rest f) [] [])]
     ; (dorun (map (comp println format-answer)))
     ))
 
