3846af1b90bff512e15392d6759ae4f02a3ec15b
Kanwei Li <kanwei@gmail.com>
2016-07-17 04:51:25 -0400

    Refactor

--- missing_numbers/Main.hs
+++ missing_numbers/Main.hs
@@ -2,25 +2,25 @@
 
 module Main where
 import Data.List
-import qualified Data.Set as S
 import qualified Data.Map as M
-import qualified Data.ByteString as BS
-import qualified Data.Text.Lazy as L
-import qualified Data.Text.Lazy.IO as LIO
+-- import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as L
+-- import qualified Data.ByteString.IO as LIO
 
 createFreqmap [] pval acc = acc
 createFreqmap (x:xs) pval acc = createFreqmap xs pval (M.insertWith (+) x pval acc)
 
-noZeroes = M.filter (\y -> y /= 0)
+noZeroes = M.filter (/= 0)
 
-ordinalSort :: [L.Text] -> [Int]
+-- ordinalSort :: [L.Text] -> [Int]
 ordinalSort = sort . (map ((read::String->Int) .  L.unpack))
 
 main = do
     _ <- getLine
-    l1 <- LIO.getLine
+    l1 <- L.getLine
     _ <- getLine
-    l2 <- LIO.getLine
+    l2 <- L.getLine
     let freq1 = createFreqmap (L.words l1) 1 (M.fromList [])
     let freq2 = createFreqmap (L.words l2) (-1) freq1
     putStrLn $ unwords $ map show $ ordinalSort $ M.keys $ noZeroes freq2
+    
